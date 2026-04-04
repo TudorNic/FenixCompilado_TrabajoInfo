d2a8e184b137f0d3ff2be8a33312880aa84eb391
FranFigueroaNombela <fjesusfn@gmail.com>
2026-04-04 09:34:09 +0200

    pruebas de tablero

--- src/ArchonFootball/ArchonFootball/main.cpp
+++ src/ArchonFootball/ArchonFootball/main.cpp
@@ -2,15 +2,40 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Prueba SFML");
+    const int tileSize = 64;
+    const int rows = 9;
+    const int cols = 9;
+
+    sf::RenderWindow window(sf::VideoMode(cols * tileSize, rows * tileSize), "Prueba SFML");
 
     sf::Texture tileTexture;
-    if (!tileTexture.loadFromFile("assets/board/tile_grass.png"))
+    if (!tileTexture.loadFromFile("D:/Users/fjesu/Documents/GitHub/FenixCompilado_TrabajoInfo/src/ArchonFootball/x64/Debug/assets/board/tile_grass.png"))
+    {
+        return -1;
+    }
+
+    sf::Texture selectedTexture;
+    if (!selectedTexture.loadFromFile("D:/Users/fjesu/Documents/GitHub/FenixCompilado_TrabajoInfo/src/ArchonFootball/x64/Debug/assets/board/tile_selected.png"))
+    {
+        return -1;
+    }
+
+    sf::Texture highlightTexture;
+    if (!highlightTexture.loadFromFile("D:/Users/fjesu/Documents/GitHub/FenixCompilado_TrabajoInfo/src/ArchonFootball/x64/Debug/assets/board/tile_highlight.png"))
+    {
+        return -1;
+    }
+
+    sf::Texture powerTexture;
+    if (!powerTexture.loadFromFile("D:/Users/fjesu/Documents/GitHub/FenixCompilado_TrabajoInfo/src/ArchonFootball/x64/Debug/assets/board/tile_power.png"))
     {
         return -1;
     }
 
     sf::Sprite tileSprite(tileTexture);
+    sf::Sprite selectedSprite(selectedTexture);
+    sf::Sprite highlightSprite(highlightTexture);
+    sf::Sprite powerSprite(powerTexture);
 
     while (window.isOpen())
     {
@@ -22,7 +47,28 @@ int main()
         }
 
         window.clear();
-        window.draw(tileSprite);
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                tileSprite.setPosition(col * tileSize, row * tileSize);
+                window.draw(tileSprite);
+            }
+        }
+
+        selectedSprite.setPosition(2 * tileSize, 2 * tileSize);
+        window.draw(selectedSprite);
+
+        highlightSprite.setPosition(3 * tileSize, 2 * tileSize);
+        window.draw(highlightSprite);
+
+        highlightSprite.setPosition(4 * tileSize, 2 * tileSize);
+        window.draw(highlightSprite);
+
+        powerSprite.setPosition(6 * tileSize, 4 * tileSize);
+        window.draw(powerSprite);
+
         window.display();
     }
 
